8a82baff44329b192200cbdb2bb1ca7af4ab09d1
Bernd Strehl <bernd.strehl@gmail.com>
2014-06-23 20:34:59 +0200

    background and waypoints

--- src/inf_pp/td/model/PlayArea.java
+++ src/inf_pp/td/model/PlayArea.java
@@ -28,7 +28,46 @@ public class PlayArea implements PlayAreaWayHolder{
 		
 		//TODO: remove tehse, load from somewhere (c)
 		waypoints=new ArrayList<Point>();
-		waypoints.add(new Point(2,0));
+		waypoints.add(new Point(0,1));
+		waypoints.add(new Point(1,1));
+		waypoints.add(new Point(2,1));
+		waypoints.add(new Point(3,1));
+		waypoints.add(new Point(4,1));
+		waypoints.add(new Point(5,1));
+		waypoints.add(new Point(6,1));
+		waypoints.add(new Point(7,1));
+		waypoints.add(new Point(8,1));
+		waypoints.add(new Point(8,2));
+		waypoints.add(new Point(8,3));
+		waypoints.add(new Point(7,3));
+		waypoints.add(new Point(6,3));
+		waypoints.add(new Point(5,3));
+		waypoints.add(new Point(4,3));
+		waypoints.add(new Point(3,3));
+		waypoints.add(new Point(2,3));
+		waypoints.add(new Point(1,3));
+		waypoints.add(new Point(1,4));
+		waypoints.add(new Point(1,5));
+		waypoints.add(new Point(2,5));
+		waypoints.add(new Point(3,5));
+		waypoints.add(new Point(4,5));
+		waypoints.add(new Point(5,5));
+		waypoints.add(new Point(6,5));
+		waypoints.add(new Point(7,5));
+		waypoints.add(new Point(8,5));
+		waypoints.add(new Point(8,6));
+		waypoints.add(new Point(8,7));
+		waypoints.add(new Point(7,7));
+		waypoints.add(new Point(6,7));
+		waypoints.add(new Point(5,7));
+		waypoints.add(new Point(4,7));
+		waypoints.add(new Point(3,7));
+		waypoints.add(new Point(2,7));
+		waypoints.add(new Point(1,7));
+		waypoints.add(new Point(1,8));
+		waypoints.add(new Point(1,9));
+		
+/*		waypoints.add(new Point(2,0));
 		waypoints.add(new Point(2,1));
 		waypoints.add(new Point(2,2));
 		waypoints.add(new Point(3,2));
@@ -42,7 +81,7 @@ public class PlayArea implements PlayAreaWayHolder{
 		waypoints.add(new Point(7,6));
 		waypoints.add(new Point(7,7));
 		waypoints.add(new Point(7,8));
-		waypoints.add(new Point(7,9));
+		waypoints.add(new Point(7,9));*/
 	}
 
 	public ArrayList<Point> getWaypoints() {
